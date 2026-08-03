dca853768a03c8411c6208b261c84d4a334c4c93
Roy Nielsen <roy.nielsen17@gmail.com>
2026-08-03 12:49:10 -0600

    fixes to the pipelines ui

--- src/jenkinsTools/ux/pipelinesMain.py
+++ src/jenkinsTools/ux/pipelinesMain.py
@@ -15,13 +15,45 @@ from PySide6.QtCore import Qt
 # Important:
 # You need to run the following command to generate the ui_form.py file
 #     pyside6-uic form.ui -o ui_form.py
-from ux.ui_nodesDialog import Ui_Dialog
+from ux.ui_pipelinesDialog import Ui_Dialog
 
 class pipelinesDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+
+        # Set current (default) page in the stacked widget
+        self.ui.stackedWidget.setCurrentIndex(0)
+
+        # Set current(default) page of combo boxes
+        self.ui.ActionComboBox.setCurrentIndex(0)
+        self.ui.MethodComboBox.setCurrentIndex(0)
+
+        # combo box actions
+        self.ui.ActionComboBox.currentIndexChanged.connect(self.handleActionComboBoxChange)
+        self.ui.MethodComboBox.currentIndexChanged.connect(self.handleMethodComboBoxChange)
+
+        # Button actions
+        self.ui.QuitPushButton.clicked.connect(self.close)
+        self.ui.RunPushButton.clicked.connect(self.runAction)
+
+    def runAction(self):
+        print(f"Running command '{self.ui.ActionComboBox.currentText()}'")
+
+    def handleActionComboBoxChange(self):
+
+        selected_text = self.ui.ActionComboBox.currentText()
+
+        print(f"selected text: '{selected_text}'")
+
+    def handleMethodComboBoxChange(self):
+
+        selected_text = self.ui.MethodComboBox.currentText()
+
+        print(f"selected text: '{selected_text}'")
+
+
 
 
 
--- src/jenkinsTools/ux/ui_pipelinesDialog.py
+++ src/jenkinsTools/ux/ui_pipelinesDialog.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'pipelineDialog.ui'
+## Form generated from reading UI file 'pipelinesDialog.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.11.1
 ##
