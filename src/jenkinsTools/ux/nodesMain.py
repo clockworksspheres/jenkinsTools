4ec24f73b65dbfb42ba3f37b61afec438a5c5f79
Roy Nielsen <roy.nielsen17@gmail.com>
2026-08-03 08:01:45 -0600

    fixing comment

--- src/jenkinsTools/ux/nodesMain.py
+++ src/jenkinsTools/ux/nodesMain.py
@@ -14,8 +14,7 @@ from PySide6.QtCore import Qt
 
 # Important:
 # You need to run the following command to generate the ui_form.py file
-#     pyside6-uic form.ui -o ui_form.py, or
-#     pyside2-uic form.ui -o ui_form.py
+#     pyside6-uic form.ui -o ui_form.py
 from ux.ui_nodesDialog import Ui_Dialog
 
 class nodesDialog(QDialog):
