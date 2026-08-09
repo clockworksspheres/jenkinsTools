3796879a4c3c79a5c6bb316ed89a6dcd48104c4c
Roy Nielsen <roy.nielsen17@gmail.com>
2026-08-09 06:23:57 -0600

    nodesMain.py: gridified, had to separately gridify each page in the stack as well

--- src/jenkinsTools/ux/ui_nodesDialog.py
+++ src/jenkinsTools/ux/ui_nodesDialog.py
@@ -17,7 +17,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QComboBox, QDialog, QGridLayout,
     QLabel, QLineEdit, QPushButton, QSizePolicy,
-    QStackedWidget, QTextEdit, QWidget)
+    QSpacerItem, QStackedWidget, QTextEdit, QWidget)
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
@@ -97,117 +97,225 @@ class Ui_Dialog(object):
         self.stackedWidget.setObjectName(u"stackedWidget")
         self.page = QWidget()
         self.page.setObjectName(u"page")
-        self.VmNamelabel = QLabel(self.page)
-        self.VmNamelabel.setObjectName(u"VmNamelabel")
-        self.VmNamelabel.setGeometry(QRect(10, 60, 121, 16))
-        self.RemoteFsLlabel = QLabel(self.page)
-        self.RemoteFsLlabel.setObjectName(u"RemoteFsLlabel")
-        self.RemoteFsLlabel.setGeometry(QRect(380, 150, 121, 16))
-        self.HostnameIpLabel = QLabel(self.page)
-        self.HostnameIpLabel.setObjectName(u"HostnameIpLabel")
-        self.HostnameIpLabel.setGeometry(QRect(10, 90, 121, 16))
+        self.gridLayout_2 = QGridLayout(self.page)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
         self.JenkinsCredsLabel = QLabel(self.page)
         self.JenkinsCredsLabel.setObjectName(u"JenkinsCredsLabel")
-        self.JenkinsCredsLabel.setGeometry(QRect(380, 60, 141, 16))
-        self.LabelsLabel = QLabel(self.page)
-        self.LabelsLabel.setObjectName(u"LabelsLabel")
-        self.LabelsLabel.setGeometry(QRect(10, 120, 121, 16))
+
+        self.gridLayout_2.addWidget(self.JenkinsCredsLabel, 0, 2, 1, 1)
+
         self.ExecutorsLabel = QLabel(self.page)
         self.ExecutorsLabel.setObjectName(u"ExecutorsLabel")
-        self.ExecutorsLabel.setGeometry(QRect(10, 150, 121, 16))
-        self.PortLabel = QLabel(self.page)
-        self.PortLabel.setObjectName(u"PortLabel")
-        self.PortLabel.setGeometry(QRect(380, 90, 141, 16))
-        self.DescriptionLabel = QLabel(self.page)
-        self.DescriptionLabel.setObjectName(u"DescriptionLabel")
-        self.DescriptionLabel.setGeometry(QRect(380, 120, 141, 16))
-        self.VmNameLineEdit = QLineEdit(self.page)
-        self.VmNameLineEdit.setObjectName(u"VmNameLineEdit")
-        self.VmNameLineEdit.setGeometry(QRect(152, 60, 191, 20))
-        self.HostnameOrIpLineEdit = QLineEdit(self.page)
-        self.HostnameOrIpLineEdit.setObjectName(u"HostnameOrIpLineEdit")
-        self.HostnameOrIpLineEdit.setGeometry(QRect(152, 90, 191, 20))
-        self.LabelsLineEdit = QLineEdit(self.page)
-        self.LabelsLineEdit.setObjectName(u"LabelsLineEdit")
-        self.LabelsLineEdit.setGeometry(QRect(152, 120, 191, 20))
-        self.ExecutorsLineEdit = QLineEdit(self.page)
-        self.ExecutorsLineEdit.setObjectName(u"ExecutorsLineEdit")
-        self.ExecutorsLineEdit.setGeometry(QRect(152, 150, 191, 20))
-        self.JenkinsCredsIdLineEdit = QLineEdit(self.page)
-        self.JenkinsCredsIdLineEdit.setObjectName(u"JenkinsCredsIdLineEdit")
-        self.JenkinsCredsIdLineEdit.setGeometry(QRect(550, 60, 171, 16))
-        self.PortLlineEdit = QLineEdit(self.page)
-        self.PortLlineEdit.setObjectName(u"PortLlineEdit")
-        self.PortLlineEdit.setGeometry(QRect(550, 90, 171, 16))
-        self.DescriptionLineEdit = QLineEdit(self.page)
-        self.DescriptionLineEdit.setObjectName(u"DescriptionLineEdit")
-        self.DescriptionLineEdit.setGeometry(QRect(550, 120, 171, 16))
+
+        self.gridLayout_2.addWidget(self.ExecutorsLabel, 3, 0, 1, 1)
+
         self.RemoteFsLineEdit = QLineEdit(self.page)
         self.RemoteFsLineEdit.setObjectName(u"RemoteFsLineEdit")
-        self.RemoteFsLineEdit.setGeometry(QRect(550, 150, 171, 16))
+
+        self.gridLayout_2.addWidget(self.RemoteFsLineEdit, 3, 3, 1, 1)
+
+        self.RemoteFsLlabel = QLabel(self.page)
+        self.RemoteFsLlabel.setObjectName(u"RemoteFsLlabel")
+
+        self.gridLayout_2.addWidget(self.RemoteFsLlabel, 3, 2, 1, 1)
+
+        self.HostnameOrIpLineEdit = QLineEdit(self.page)
+        self.HostnameOrIpLineEdit.setObjectName(u"HostnameOrIpLineEdit")
+
+        self.gridLayout_2.addWidget(self.HostnameOrIpLineEdit, 1, 1, 1, 1)
+
+        self.PortLlineEdit = QLineEdit(self.page)
+        self.PortLlineEdit.setObjectName(u"PortLlineEdit")
+
+        self.gridLayout_2.addWidget(self.PortLlineEdit, 1, 3, 1, 1)
+
+        self.LabelsLineEdit = QLineEdit(self.page)
+        self.LabelsLineEdit.setObjectName(u"LabelsLineEdit")
+
+        self.gridLayout_2.addWidget(self.LabelsLineEdit, 2, 1, 1, 1)
+
+        self.VmNameLineEdit = QLineEdit(self.page)
+        self.VmNameLineEdit.setObjectName(u"VmNameLineEdit")
+
+        self.gridLayout_2.addWidget(self.VmNameLineEdit, 0, 1, 1, 1)
+
+        self.HostnameIpLabel = QLabel(self.page)
+        self.HostnameIpLabel.setObjectName(u"HostnameIpLabel")
+
+        self.gridLayout_2.addWidget(self.HostnameIpLabel, 1, 0, 1, 1)
+
+        self.JenkinsCredsIdLineEdit = QLineEdit(self.page)
+        self.JenkinsCredsIdLineEdit.setObjectName(u"JenkinsCredsIdLineEdit")
+
+        self.gridLayout_2.addWidget(self.JenkinsCredsIdLineEdit, 0, 3, 1, 1)
+
+        self.VmNamelabel = QLabel(self.page)
+        self.VmNamelabel.setObjectName(u"VmNamelabel")
+
+        self.gridLayout_2.addWidget(self.VmNamelabel, 0, 0, 1, 1)
+
+        self.LabelsLabel = QLabel(self.page)
+        self.LabelsLabel.setObjectName(u"LabelsLabel")
+
+        self.gridLayout_2.addWidget(self.LabelsLabel, 2, 0, 1, 1)
+
+        self.PortLabel = QLabel(self.page)
+        self.PortLabel.setObjectName(u"PortLabel")
+
+        self.gridLayout_2.addWidget(self.PortLabel, 1, 2, 1, 1)
+
+        self.DescriptionLineEdit = QLineEdit(self.page)
+        self.DescriptionLineEdit.setObjectName(u"DescriptionLineEdit")
+
+        self.gridLayout_2.addWidget(self.DescriptionLineEdit, 2, 3, 1, 1)
+
+        self.DescriptionLabel = QLabel(self.page)
+        self.DescriptionLabel.setObjectName(u"DescriptionLabel")
+
+        self.gridLayout_2.addWidget(self.DescriptionLabel, 2, 2, 1, 1)
+
+        self.ExecutorsLineEdit = QLineEdit(self.page)
+        self.ExecutorsLineEdit.setObjectName(u"ExecutorsLineEdit")
+
+        self.gridLayout_2.addWidget(self.ExecutorsLineEdit, 3, 1, 1, 1)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.gridLayout_2.addItem(self.verticalSpacer, 4, 1, 1, 1)
+
+        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.gridLayout_2.addItem(self.verticalSpacer_2, 4, 0, 1, 1)
+
+        self.verticalSpacer_3 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.gridLayout_2.addItem(self.verticalSpacer_3, 4, 2, 1, 1)
+
+        self.verticalSpacer_4 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.gridLayout_2.addItem(self.verticalSpacer_4, 4, 3, 1, 1)
+
         self.stackedWidget.addWidget(self.page)
         self.page_2 = QWidget()
         self.page_2.setObjectName(u"page_2")
-        self.JVMOptionsLineEdit = QLineEdit(self.page_2)
-        self.JVMOptionsLineEdit.setObjectName(u"JVMOptionsLineEdit")
-        self.JVMOptionsLineEdit.setGeometry(QRect(490, 60, 231, 20))
-        self.RemoteFsLlabel_2 = QLabel(self.page_2)
-        self.RemoteFsLlabel_2.setObjectName(u"RemoteFsLlabel_2")
-        self.RemoteFsLlabel_2.setGeometry(QRect(350, 150, 121, 16))
-        self.VmNameLineEdit_2 = QLineEdit(self.page_2)
-        self.VmNameLineEdit_2.setObjectName(u"VmNameLineEdit_2")
-        self.VmNameLineEdit_2.setGeometry(QRect(152, 60, 171, 20))
-        self.JenkinsCredsLabel_2 = QLabel(self.page_2)
-        self.JenkinsCredsLabel_2.setObjectName(u"JenkinsCredsLabel_2")
-        self.JenkinsCredsLabel_2.setGeometry(QRect(350, 60, 121, 16))
-        self.HostnameIpLabel_2 = QLabel(self.page_2)
-        self.HostnameIpLabel_2.setObjectName(u"HostnameIpLabel_2")
-        self.HostnameIpLabel_2.setGeometry(QRect(10, 90, 91, 16))
+        self.gridLayout_3 = QGridLayout(self.page_2)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
         self.LabelsLineEdit_2 = QLineEdit(self.page_2)
         self.LabelsLineEdit_2.setObjectName(u"LabelsLineEdit_2")
-        self.LabelsLineEdit_2.setGeometry(QRect(152, 120, 171, 20))
-        self.HostnameOrIpLineEdit_2 = QLineEdit(self.page_2)
-        self.HostnameOrIpLineEdit_2.setObjectName(u"HostnameOrIpLineEdit_2")
-        self.HostnameOrIpLineEdit_2.setGeometry(QRect(152, 90, 171, 20))
-        self.DescriptionLabel_2 = QLabel(self.page_2)
-        self.DescriptionLabel_2.setObjectName(u"DescriptionLabel_2")
-        self.DescriptionLabel_2.setGeometry(QRect(350, 120, 121, 16))
+
+        self.gridLayout_3.addWidget(self.LabelsLineEdit_2, 2, 1, 1, 1)
+
         self.VmNamelabel_2 = QLabel(self.page_2)
         self.VmNamelabel_2.setObjectName(u"VmNamelabel_2")
-        self.VmNamelabel_2.setGeometry(QRect(10, 60, 91, 16))
-        self.LabelsLabel_2 = QLabel(self.page_2)
-        self.LabelsLabel_2.setObjectName(u"LabelsLabel_2")
-        self.LabelsLabel_2.setGeometry(QRect(10, 120, 91, 16))
-        self.ExecutorsLabel_2 = QLabel(self.page_2)
-        self.ExecutorsLabel_2.setObjectName(u"ExecutorsLabel_2")
-        self.ExecutorsLabel_2.setGeometry(QRect(10, 150, 91, 16))
-        self.ExecutorsLineEdit_2 = QLineEdit(self.page_2)
-        self.ExecutorsLineEdit_2.setObjectName(u"ExecutorsLineEdit_2")
-        self.ExecutorsLineEdit_2.setGeometry(QRect(152, 150, 171, 20))
-        self.RemoteFsLineEdit_2 = QLineEdit(self.page_2)
-        self.RemoteFsLineEdit_2.setObjectName(u"RemoteFsLineEdit_2")
-        self.RemoteFsLineEdit_2.setGeometry(QRect(490, 150, 231, 20))
+
+        self.gridLayout_3.addWidget(self.VmNamelabel_2, 0, 0, 1, 1)
+
+        self.JVMOptionsLineEdit = QLineEdit(self.page_2)
+        self.JVMOptionsLineEdit.setObjectName(u"JVMOptionsLineEdit")
+
+        self.gridLayout_3.addWidget(self.JVMOptionsLineEdit, 0, 3, 1, 1)
+
+        self.HostnameIpLabel_2 = QLabel(self.page_2)
+        self.HostnameIpLabel_2.setObjectName(u"HostnameIpLabel_2")
+
+        self.gridLayout_3.addWidget(self.HostnameIpLabel_2, 1, 0, 1, 1)
+
+        self.HostnameOrIpLineEdit_2 = QLineEdit(self.page_2)
+        self.HostnameOrIpLineEdit_2.setObjectName(u"HostnameOrIpLineEdit_2")
+
+        self.gridLayout_3.addWidget(self.HostnameOrIpLineEdit_2, 1, 1, 1, 1)
+
         self.DescriptionLineEdit_2 = QLineEdit(self.page_2)
         self.DescriptionLineEdit_2.setObjectName(u"DescriptionLineEdit_2")
-        self.DescriptionLineEdit_2.setGeometry(QRect(490, 120, 231, 20))
+
+        self.gridLayout_3.addWidget(self.DescriptionLineEdit_2, 2, 3, 1, 1)
+
+        self.ExecutorsLineEdit_2 = QLineEdit(self.page_2)
+        self.ExecutorsLineEdit_2.setObjectName(u"ExecutorsLineEdit_2")
+
+        self.gridLayout_3.addWidget(self.ExecutorsLineEdit_2, 3, 1, 1, 1)
+
+        self.LabelsLabel_2 = QLabel(self.page_2)
+        self.LabelsLabel_2.setObjectName(u"LabelsLabel_2")
+
+        self.gridLayout_3.addWidget(self.LabelsLabel_2, 2, 0, 1, 1)
+
+        self.JenkinsCredsLabel_2 = QLabel(self.page_2)
+        self.JenkinsCredsLabel_2.setObjectName(u"JenkinsCredsLabel_2")
+
+        self.gridLayout_3.addWidget(self.JenkinsCredsLabel_2, 0, 2, 1, 1)
+
+        self.VmNameLineEdit_2 = QLineEdit(self.page_2)
+        self.VmNameLineEdit_2.setObjectName(u"VmNameLineEdit_2")
+
+        self.gridLayout_3.addWidget(self.VmNameLineEdit_2, 0, 1, 1, 1)
+
+        self.RemoteFsLineEdit_2 = QLineEdit(self.page_2)
+        self.RemoteFsLineEdit_2.setObjectName(u"RemoteFsLineEdit_2")
+
+        self.gridLayout_3.addWidget(self.RemoteFsLineEdit_2, 3, 3, 1, 1)
+
+        self.ExecutorsLabel_2 = QLabel(self.page_2)
+        self.ExecutorsLabel_2.setObjectName(u"ExecutorsLabel_2")
+
+        self.gridLayout_3.addWidget(self.ExecutorsLabel_2, 3, 0, 1, 1)
+
+        self.DescriptionLabel_2 = QLabel(self.page_2)
+        self.DescriptionLabel_2.setObjectName(u"DescriptionLabel_2")
+
+        self.gridLayout_3.addWidget(self.DescriptionLabel_2, 2, 2, 1, 1)
+
+        self.RemoteFsLlabel_2 = QLabel(self.page_2)
+        self.RemoteFsLlabel_2.setObjectName(u"RemoteFsLlabel_2")
+
+        self.gridLayout_3.addWidget(self.RemoteFsLlabel_2, 3, 2, 1, 1)
+
+        self.verticalSpacer_5 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.gridLayout_3.addItem(self.verticalSpacer_5, 4, 1, 1, 1)
+
+        self.verticalSpacer_6 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.gridLayout_3.addItem(self.verticalSpacer_6, 4, 0, 1, 1)
+
+        self.verticalSpacer_7 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.gridLayout_3.addItem(self.verticalSpacer_7, 4, 2, 1, 1)
+
+        self.verticalSpacer_8 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.gridLayout_3.addItem(self.verticalSpacer_8, 4, 3, 1, 1)
+
         self.stackedWidget.addWidget(self.page_2)
         self.page_3 = QWidget()
         self.page_3.setObjectName(u"page_3")
+        self.gridLayout_4 = QGridLayout(self.page_3)
+        self.gridLayout_4.setObjectName(u"gridLayout_4")
         self.GetNodesTextEdit = QTextEdit(self.page_3)
         self.GetNodesTextEdit.setObjectName(u"GetNodesTextEdit")
-        self.GetNodesTextEdit.setGeometry(QRect(20, 10, 671, 241))
+
+        self.gridLayout_4.addWidget(self.GetNodesTextEdit, 0, 0, 1, 1)
+
         self.stackedWidget.addWidget(self.page_3)
         self.page_4 = QWidget()
         self.page_4.setObjectName(u"page_4")
+        self.gridLayout_5 = QGridLayout(self.page_4)
+        self.gridLayout_5.setObjectName(u"gridLayout_5")
         self.VmNamelabel_3 = QLabel(self.page_4)
         self.VmNamelabel_3.setObjectName(u"VmNamelabel_3")
-        self.VmNamelabel_3.setGeometry(QRect(10, 60, 121, 16))
+
+        self.gridLayout_5.addWidget(self.VmNamelabel_3, 0, 0, 1, 1)
+
         self.VmNameLineEdit_3 = QLineEdit(self.page_4)
         self.VmNameLineEdit_3.setObjectName(u"VmNameLineEdit_3")
-        self.VmNameLineEdit_3.setGeometry(QRect(152, 60, 131, 20))
+
+        self.gridLayout_5.addWidget(self.VmNameLineEdit_3, 0, 1, 1, 1)
+
         self.GetNodeTextEdit = QTextEdit(self.page_4)
         self.GetNodeTextEdit.setObjectName(u"GetNodeTextEdit")
-        self.GetNodeTextEdit.setGeometry(QRect(10, 100, 701, 151))
+
+        self.gridLayout_5.addWidget(self.GetNodeTextEdit, 1, 0, 1, 2)
+
         self.stackedWidget.addWidget(self.page_4)
 
         self.gridLayout.addWidget(self.stackedWidget, 5, 0, 1, 5)
@@ -228,7 +336,7 @@ class Ui_Dialog(object):
 
         self.retranslateUi(Dialog)
 
-        self.stackedWidget.setCurrentIndex(0)
+        self.stackedWidget.setCurrentIndex(3)
 
 
         QMetaObject.connectSlotsByName(Dialog)
@@ -257,22 +365,22 @@ class Ui_Dialog(object):
         self.MethodComboBox.setItemText(0, QCoreApplication.translate("Dialog", u"SSH", None))
         self.MethodComboBox.setItemText(1, QCoreApplication.translate("Dialog", u"JNLP", None))
 
-        self.VmNamelabel.setText(QCoreApplication.translate("Dialog", u"VM Name", None))
-        self.RemoteFsLlabel.setText(QCoreApplication.translate("Dialog", u"Remote Filesystem", None))
-        self.HostnameIpLabel.setText(QCoreApplication.translate("Dialog", u"Hostname/IP", None))
         self.JenkinsCredsLabel.setText(QCoreApplication.translate("Dialog", u"Jenkins Credentials ID", None))
-        self.LabelsLabel.setText(QCoreApplication.translate("Dialog", u"Labels", None))
         self.ExecutorsLabel.setText(QCoreApplication.translate("Dialog", u"Executors", None))
+        self.RemoteFsLlabel.setText(QCoreApplication.translate("Dialog", u"Remote Filesystem", None))
+        self.PortLlineEdit.setText(QCoreApplication.translate("Dialog", u"22", None))
+        self.HostnameIpLabel.setText(QCoreApplication.translate("Dialog", u"Hostname/IP", None))
+        self.VmNamelabel.setText(QCoreApplication.translate("Dialog", u"VM Name", None))
+        self.LabelsLabel.setText(QCoreApplication.translate("Dialog", u"Labels", None))
         self.PortLabel.setText(QCoreApplication.translate("Dialog", u"Port", None))
         self.DescriptionLabel.setText(QCoreApplication.translate("Dialog", u"Description", None))
-        self.PortLlineEdit.setText(QCoreApplication.translate("Dialog", u"22", None))
-        self.RemoteFsLlabel_2.setText(QCoreApplication.translate("Dialog", u"Remote Filesystem", None))
-        self.JenkinsCredsLabel_2.setText(QCoreApplication.translate("Dialog", u"JVM Options", None))
-        self.HostnameIpLabel_2.setText(QCoreApplication.translate("Dialog", u"Hostname/IP", None))
-        self.DescriptionLabel_2.setText(QCoreApplication.translate("Dialog", u"Description", None))
         self.VmNamelabel_2.setText(QCoreApplication.translate("Dialog", u"VM Name", None))
+        self.HostnameIpLabel_2.setText(QCoreApplication.translate("Dialog", u"Hostname/IP", None))
         self.LabelsLabel_2.setText(QCoreApplication.translate("Dialog", u"Labels", None))
+        self.JenkinsCredsLabel_2.setText(QCoreApplication.translate("Dialog", u"JVM Options", None))
         self.ExecutorsLabel_2.setText(QCoreApplication.translate("Dialog", u"Executors", None))
+        self.DescriptionLabel_2.setText(QCoreApplication.translate("Dialog", u"Description", None))
+        self.RemoteFsLlabel_2.setText(QCoreApplication.translate("Dialog", u"Remote Filesystem", None))
         self.VmNamelabel_3.setText(QCoreApplication.translate("Dialog", u"VM Name", None))
         self.label.setText("")
     # retranslateUi
