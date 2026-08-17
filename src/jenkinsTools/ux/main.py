721020ab93ec5c501aa2d4a128c2ea0eba99a722
Roy Nielsen <roy.nielsen17@gmail.com>
2026-08-17 17:31:06 -0600

    splitting out functionality to a separate file - no longer using that functionality in main.py

--- src/jenkinsTools/ux/main.py
+++ src/jenkinsTools/ux/main.py
@@ -17,57 +17,6 @@ from ux.pipelinesMain import pipelinesDialog
 from ux.consoleDialog import ConsoleStream, ConsoleDialog
 
 
-class CustomDialog(QDialog):
-    def __init__(self, parent=None):
-        """
-        Not yet implemented error dialog
-        """
-        super().__init__(parent)
-        self.setWindowTitle("Oops")
-
-        self.button = QPushButton("Close")
-        self.button.clicked.connect(self.accept)
-        layout = QVBoxLayout()
-        message = QLabel("Not Yet Implemented")
-        layout.addWidget(message)
-        layout.addWidget(self.button)
-        self.setLayout(layout)
-
-
-class CustomMessageDialog(QDialog):
-    def __init__(self, parent=None, message=""):
-        """
-        Generic message dialog that shows the passed in message.
-        """
-        super().__init__(parent)
-
-        # Set window title
-        self.setWindowTitle("message")
-
-        # Create layout
-        layout = QVBoxLayout()
-
-        # Add centered message label
-        messageText = QLabel(f"{message}")
-        messageText.setAlignment(Qt.AlignLeft)
-        layout.addWidget(messageText)
-
-        # Add single OK button
-        button_box = QDialogButtonBox(QDialogButtonBox.Ok)
-        layout.addWidget(button_box)
-
-        # Connect the OK button to the accept slot
-        button_box.accepted.connect(self.accept)
-        '''
-        self.button = QPushButton("Ok")
-        self.button.setDefault(True)
-        self.button.accepted.connect(self.accept)
-        layout.addWidget(self.button)
-        '''
-        # Set the layout
-        self.setLayout(layout)
-
-
 class JenkinsToolsUi(QMainWindow):
     def __init__(self):
         super().__init__()
